e01b18d0b08d9cdf861197032259426083274407
Yousong Zhou <yszhou4tech@gmail.com>
2025-03-06 00:23:12 +0800

    load_file: load tensors ordered by their offsets (#571)
    (cherry picked from commit 467a605bcb0f8ce3a324b9e1dd93c309bd6661aa)

--- bindings/python/py_src/safetensors/torch.py
+++ bindings/python/py_src/safetensors/torch.py
@@ -306,7 +306,7 @@ def load_file(filename: Union[str, os.PathLike], device="cpu") -> Dict[str, torc
     """
     result = {}
     with safe_open(filename, framework="pt", device=device) as f:
-        for k in f.keys():
+        for k in f.offset_keys():
             result[k] = f.get_tensor(k)
     return result
 
